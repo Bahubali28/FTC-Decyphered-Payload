13c4c7d9d456922d83bc178666a893eebb91f2f4
Ayaansh Mahendra <144401359+Bahubali28@users.noreply.github.com>
2023-11-03 18:25:18 -0500

    Updated APK Change #1

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptTensorFlowObjectDetection.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptTensorFlowObjectDetection.java
@@ -29,7 +29,8 @@
 
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import android.util.Size;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
@@ -48,17 +49,16 @@ import java.util.List;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
 @TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
-@Disabled
 public class ConceptTensorFlowObjectDetection extends LinearOpMode {
 
-    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+    private static final boolean USE_WEBCAM = false;  // true for webcam, false for phone camera
 
     // TFOD_MODEL_ASSET points to a model file stored in the project Asset location,
     // this is only used for Android Studio when using models in Assets.
-    private static final String TFOD_MODEL_ASSET = "MyModelStoredAsAsset.tflite";
+    private static final String TFOD_MODEL_ASSET = "CenterStage.tflite";
     // TFOD_MODEL_FILE points to a model file stored onboard the Robot Controller's storage,
     // this is used when uploading models directly to the RC using the model upload interface.
-    private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/myCustomModel.tflite";
+    //private static final String TFOD_MODEL_FILE = "src/main/assets/CenterStage.tflite";
     // Define the labels recognized in the model for TFOD (must be in training order!)
     private static final String[] LABELS = {
        "Pixel",
@@ -123,12 +123,12 @@ public class ConceptTensorFlowObjectDetection extends LinearOpMode {
             // choose one of the following:
             //   Use setModelAssetName() if the custom TF Model is built in as an asset (AS only).
             //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
-            //.setModelAssetName(TFOD_MODEL_ASSET)
+            .setModelAssetName(TFOD_MODEL_ASSET)
             //.setModelFileName(TFOD_MODEL_FILE)
 
             // The following default settings are available to un-comment and edit as needed to 
             // set parameters for custom models.
-            //.setModelLabels(LABELS)
+            .setModelLabels(LABELS)
             //.setIsModelTensorFlow2(true)
             //.setIsModelQuantized(true)
             //.setModelInputSize(300)
@@ -147,10 +147,10 @@ public class ConceptTensorFlowObjectDetection extends LinearOpMode {
         }
 
         // Choose a camera resolution. Not all cameras support all resolutions.
-        //builder.setCameraResolution(new Size(640, 480));
+        builder.setCameraResolution(new Size(640, 480));
 
         // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
-        //builder.enableLiveView(true);
+        builder.enableLiveView(true);
 
         // Set the stream format; MJPEG uses less bandwidth than default YUY2.
         //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
@@ -158,7 +158,7 @@ public class ConceptTensorFlowObjectDetection extends LinearOpMode {
         // Choose whether or not LiveView stops if no processors are enabled.
         // If set "true", monitor shows solid orange screen if no processors enabled.
         // If set "false", monitor shows camera view without annotations.
-        //builder.setAutoStopLiveView(false);
+        builder.setAutoStopLiveView(false);
 
         // Set and enable the processor.
         builder.addProcessor(tfod);
@@ -167,10 +167,10 @@ public class ConceptTensorFlowObjectDetection extends LinearOpMode {
         visionPortal = builder.build();
 
         // Set confidence threshold for TFOD recognitions, at any time.
-        //tfod.setMinResultConfidence(0.75f);
+        tfod.setMinResultConfidence(0.75f);
 
         // Disable or re-enable the TFOD processor at any time.
-        //visionPortal.setProcessorEnabled(tfod, true);
+        visionPortal.setProcessorEnabled(tfod, true);
 
     }   // end method initTfod()
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp.java
@@ -4,26 +4,29 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import java.util.concurrent.TimeUnit;
+
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp
 
 public class TeleOp extends LinearOpMode {
-    private DcMotor fr, fl, br, bl, dcAc1, dcAc2;
+    private DcMotor fr, fl, br, bl, dcAc1, dcAc2, dcIn1, dcIn2;
     private Servo serAc1, serAc2;
 
     public void runOpMode() {
         fl = hardwareMap.get(DcMotor.class, "fl");
-        br = hardwareMap.get(DcMotor.class,"br");
-        fr = hardwareMap.get(DcMotor.class,"fr");
-        bl = hardwareMap.get(DcMotor.class,"bl");
+        br = hardwareMap.get(DcMotor.class, "br");
+        fr = hardwareMap.get(DcMotor.class, "fr");
+        bl = hardwareMap.get(DcMotor.class, "bl");
         dcAc1 = hardwareMap.get(DcMotor.class, "dcAc1");
         dcAc2 = hardwareMap.get(DcMotor.class, "dcAc2");
+        dcIn1 = hardwareMap.get(DcMotor.class, "dcIn1");
+        dcIn2 = hardwareMap.get(DcMotor.class, "dcIn2");
         serAc1 = hardwareMap.get(Servo.class, "serAc1");
         serAc2 = hardwareMap.get(Servo.class, "serAc2");
-
         bl.setDirection(DcMotorSimple.Direction.REVERSE);
         fl.setDirection(DcMotorSimple.Direction.REVERSE);
         serAc2.setDirection(Servo.Direction.REVERSE);
-
         serAc1.setPosition(0);
         serAc2.setPosition(0);
         telemetry.addLine("Motors Assigned and Attached");
@@ -36,19 +39,27 @@ public class TeleOp extends LinearOpMode {
             double lx = gamepad1.left_stick_x;
             double ly = gamepad1.left_stick_y;
             double rx = gamepad1.right_stick_x;
-
+            double rt = gamepad2.right_trigger;
+            double lt = gamepad2.left_trigger;
+            double ly2 = gamepad2.left_stick_y;
+            double ry2 = gamepad2.right_stick_y;
             fl.setPower(ly + lx - rx);
             fr.setPower(ly - lx + rx);
             bl.setPower(ly - lx - rx);
             br.setPower(ly + lx + rx);
+            dcIn2.setPower(-rt);
+            dcIn2.setPower(lt);
+            dcAc1.setPower(-ry2);
+            dcAc2.setPower(-ly2);
+
             if (gamepad1.dpad_up || gamepad2.dpad_up == true) {
                 dcAc1.setPower(1);
                 dcAc2.setPower(1);
-                }
-                if (gamepad2.dpad_up == false) {
-                    dcAc1.setPower(0);
-                    dcAc2.setPower(0);
-                }
+            }
+            if (gamepad2.dpad_up == false) {
+                dcAc1.setPower(0);
+                dcAc2.setPower(0);
+            }
             if (gamepad2.dpad_down == true) {
                 while (gamepad2.dpad_down == true) {
                     dcAc1.setPower(-1);
@@ -60,12 +71,32 @@ public class TeleOp extends LinearOpMode {
                 }
             }
             if (gamepad2.y == true) {
-                    serAc1.setPosition(0.35);
-                    serAc2.setPosition(0.35);
+                serAc1.setPosition(0.35);
+                serAc2.setPosition(0.35);
             }
             if (gamepad2.a == true) {
                 serAc1.setPosition(0);
                 serAc2.setPosition(0);
+            }
+            if (gamepad2.right_bumper == true) {
+                try {
+                    dcIn1.setPower(1);
+                    TimeUnit.MILLISECONDS.sleep(80);
+                    dcIn1.setPower(0);
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (Exception e) {
+                    //TODO: handle exception
+                }
+                if (gamepad2.left_bumper == true) {
+                    try {
+                        dcIn1.setPower(-1);
+                        TimeUnit.MILLISECONDS.sleep(80);
+                        dcIn1.setPower(0);
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (Exception e) {
+                        //TODO: handle exception
+                    }
+                }
             }
         }
     }
