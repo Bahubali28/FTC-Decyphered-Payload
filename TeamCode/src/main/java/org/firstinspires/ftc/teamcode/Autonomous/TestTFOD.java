cdcd60e9aeac15cb500bcf423d5e99e4ab933c63
Ayaansh Mahendra <144401359+Bahubali28@users.noreply.github.com>
2024-01-25 20:39:27 -0600

    TestTFOD updated to prevent Syntax Error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestTFOD.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestTFOD.java
@@ -460,7 +460,38 @@ public class TestTFOD extends LinearOpMode {
      * Initialize the TensorFlow Object Detection processor.
      */
     private void initVisionPortal(VisionProcessor processor) {
+        // Create the vision portal by using a builder.
+        VisionPortal.Builder builder = new VisionPortal.Builder();
 
+        // Set the camera (webcam vs. built-in RC phone camera).
+        if (USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        } else {
+            builder.setCamera(BuiltinCameraDirection.BACK);
+        }
+
+        // Choose a camera resolution. Not all cameras support all resolutions.
+        builder.setCameraResolution(new Size(800, 448));
+
+        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+        builder.enableLiveView(true);
+
+        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
+        builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+
+        // Choose whether or not LiveView stops if no processors are enabled.
+        // If set "true", monitor shows solid orange screen if no processors enabled.
+        // If set "false", monitor shows camera view without annotations.
+        builder.setAutoStopLiveView(true);
+
+        // Set and enable the processor.
+        builder.addProcessor(processor);
+
+        // Build the Vision Portal, using the above settings.
+        visionPortal = builder.build();
+
+        // Disable or re-enable the TFOD processor at any time.
+        visionPortal.setProcessorEnabled(processor, true);
         // Create the TensorFlow processor by using a builder.
         if (processor == tfod) {
             tfod = new TfodProcessor.Builder()
@@ -472,7 +503,6 @@ public class TestTFOD extends LinearOpMode {
                     //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
 //                .setModelFileName(TFOD_MODEL_FILE)
                     .setModelAssetName(TFOD_MODEL_ASSET)
-
                     // The following default settings are available to un-comment and edit as needed to
                     // set parameters for custom models.
                     .setModelLabels(LABELS)
@@ -513,39 +543,6 @@ public class TestTFOD extends LinearOpMode {
             aprilTag.setDecimation(2);
             TestTFOD.aprilTagDetectionIsActive = true;
         }
-        // Create the vision portal by using a builder.
-        VisionPortal.Builder builder = new VisionPortal.Builder();
-
-        // Set the camera (webcam vs. built-in RC phone camera).
-        if (USE_WEBCAM) {
-            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
-        } else {
-            builder.setCamera(BuiltinCameraDirection.BACK);
-        }
-
-        // Choose a camera resolution. Not all cameras support all resolutions.
-        builder.setCameraResolution(new Size(800, 448));
-
-        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
-        builder.enableLiveView(true);
-
-        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
-        builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
-
-        // Choose whether or not LiveView stops if no processors are enabled.
-        // If set "true", monitor shows solid orange screen if no processors enabled.
-        // If set "false", monitor shows camera view without annotations.
-        builder.setAutoStopLiveView(true);
-
-        // Set and enable the processor.
-        builder.addProcessor(processor);
-
-        // Build the Vision Portal, using the above settings.
-        visionPortal = builder.build();
-
-        // Disable or re-enable the TFOD processor at any time.
-        visionPortal.setProcessorEnabled(processor, true);
-
     }   // end method initTfod()
 
     /**
